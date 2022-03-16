cf11c729f5e444da949eac7608e41ae002d24576
Chris Griggs <chris@macbook-pro.myfiosgateway.com>
2022-03-15 22:39:59 -0400

    Made the index file a go template to take in dynamic values

--- main.go
+++ main.go
@@ -1,23 +1,38 @@
 package main
 
-import "fmt"
-import "net/http"
-import "log"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"text/template"
+)
 
-
-func serveFiles(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(r.URL.Path)
-    p := "." + r.URL.Path
-    if p == "./" {
-        p = "./index.html"
-    }
-    http.ServeFile(w, r, p)
+type Data struct {
+	News []string
 }
 
+func serveFiles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	fileData, _ := ioutil.ReadFile("./index.tmpl")
+
+	tmpl, err := template.New("index").Parse(string(fileData))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	data := Data{
+		News: []string{
+			"Current JIRA In Progress: POPS-2538",
+			"No Blockers",
+			"Next OOO: April 1-3",
+		},
+	}
+
+	tmpl.Execute(w, data)
+}
 
 func main() {
-      http.HandleFunc("/", serveFiles)
-    log.Fatal(http.ListenAndServe(":80", nil))
-
-
+	http.HandleFunc("/", serveFiles)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
